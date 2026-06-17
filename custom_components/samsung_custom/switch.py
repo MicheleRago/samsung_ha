c7d8e2bfe74d029e479415302b9b05733aa3a2e8
Michele Rago <michele@rago.ovh>
2026-06-18 01:25:21 +0200

    Fix

--- custom_components/samsung_custom/switch.py
+++ custom_components/samsung_custom/switch.py
@@ -2,7 +2,7 @@ import logging
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from .const import DOMAIN, CAP_SWITCH
+from .const import DOMAIN, CAP_SWITCH, CAP_WASHING_OPTIONS
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -18,9 +18,16 @@ async def async_setup_entry(hass, entry, async_add_entities):
         device_name = device_info.get("name", "Samsung Appliance")
         
         for comp_name, status in components.items():
+            name_prefix = f"{device_name} ({comp_name})" if comp_name != "main" else device_name
+            
             if CAP_SWITCH in status:
-                name = f"{device_name} ({comp_name})" if comp_name != "main" else device_name
-                switches.append(GenericPowerSwitch(coordinator, device_id, comp_name, name))
+                switches.append(GenericPowerSwitch(coordinator, device_id, comp_name, name_prefix))
+                
+            if CAP_WASHING_OPTIONS in status:
+                switches.append(GenericOptionSwitch(coordinator, device_id, comp_name, name_prefix, CAP_WASHING_OPTIONS, "halfLoad", "setHalfLoad", "Half Load", "mdi:washing-machine"))
+                switches.append(GenericOptionSwitch(coordinator, device_id, comp_name, name_prefix, CAP_WASHING_OPTIONS, "speedBooster", "setSpeedBooster", "Speed Booster", "mdi:fast-forward"))
+                switches.append(GenericOptionSwitch(coordinator, device_id, comp_name, name_prefix, CAP_WASHING_OPTIONS, "sanitization", "setSanitization", "Sanitization", "mdi:water-boiler"))
+                switches.append(GenericOptionSwitch(coordinator, device_id, comp_name, name_prefix, CAP_WASHING_OPTIONS, "autoReleaseDry", "setAutoReleaseDry", "Auto Release Dry", "mdi:door-open"))
             
     async_add_entities(switches)
 
@@ -69,3 +76,51 @@ class GenericPowerSwitch(CoordinatorEntity, SwitchEntity):
         await self.coordinator.async_request_refresh()
 
 
+class GenericOptionSwitch(CoordinatorEntity, SwitchEntity):
+    """Switch for generic options."""
+
+    def __init__(self, coordinator, device_id, component, device_name, capability, attribute, command, name, icon):
+        super().__init__(coordinator)
+        self._device_id = device_id
+        self._component = component
+        self._device_name = device_name
+        self._capability = capability
+        self._attribute = attribute
+        self._command = command
+        
+        comp_prefix = f"_{component}" if component != "main" else ""
+        self._attr_unique_id = f"{device_id}{comp_prefix}_{capability}_{attribute}"
+        self._attr_name = name
+        self._attr_icon = icon
+        self._attr_has_entity_name = True
+
+    @property
+    def device_info(self):
+        return {
+            "identifiers": {(DOMAIN, self._device_id)},
+            "name": self._device_name.replace(f" ({self._component})", ""),
+            "manufacturer": "Samsung",
+        }
+
+    @property
+    def is_on(self):
+        """Return true if switch is on."""
+        data = self.coordinator.data.get(self._device_id, {}).get("status", {}).get(self._component, {})
+        if not data:
+            return False
+        val = data.get(self._capability, {}).get(self._attribute, {}).get("value")
+        return val == "on" or val == True
+
+    async def async_turn_on(self, **kwargs):
+        """Turn the option on."""
+        import asyncio
+        await self.coordinator.api.execute_command(self._device_id, self._component, self._capability, self._command, ["on"])
+        await asyncio.sleep(2)
+        await self.coordinator.async_request_refresh()
+
+    async def async_turn_off(self, **kwargs):
+        """Turn the option off."""
+        import asyncio
+        await self.coordinator.api.execute_command(self._device_id, self._component, self._capability, self._command, ["off"])
+        await asyncio.sleep(2)
+        await self.coordinator.async_request_refresh()
