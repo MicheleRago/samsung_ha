c55c1e466e5c7f6ffb1f352a878ab286fd142176
Michele Rago <michele@rago.ovh>
2026-06-18 04:15:33 +0200

    test

--- custom_components/samsung_custom/button.py
+++ custom_components/samsung_custom/button.py
@@ -81,17 +81,8 @@ class GenericCommandButton(CoordinatorEntity, ButtonEntity):
                 mode = data.get("samsungce.ovenMode", {}).get("ovenMode", {}).get("value", "Bake")
                 temp = data.get("ovenSetpoint", {}).get("ovenSetpoint", {}).get("value", 200)
                 
-                # Check for locally cached user selections before the API fully updates
-                cache_key = f"{self._device_id}_pending_oven_state"
-                if cache_key in self.coordinator.hass.data[DOMAIN]:
-                    mode = self.coordinator.hass.data[DOMAIN][cache_key].get("mode", mode)
-                    temp = self.coordinator.hass.data[DOMAIN][cache_key].get("temp", temp)
-                    
-                # Ensure time and temp are strictly integers to avoid API Error 422 (integer expected)
-                api_temp = int(float(temp))
-                
-                # Pass mode, cookTime in seconds (e.g. 0 for manual), and temperature
-                args = [mode, 0, api_temp]
+                # HARDCODED EXPERIMENT to test strict typing
+                args = ["Conventional", 0, 160]
                 
             await self.coordinator.api.execute_command(self._device_id, self._component, cap, cmd, args)
         else:
